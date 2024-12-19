12500c3a85a30ab16961831db527aa22adb4d81d
Emir Chacra <emirchacra@Emirs-MacBook-Pro.local>
2024-12-19 15:41:50 -0300

    Add example text

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,18 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p>Hi,\n
+      I'm Emir Chacra, a Chilean music producer, bassist, DJ, and digital audio researcher.</p>
+
+      <p>My webpage is under construction :) But soon it'll be online and beautiful again.</p>
+
+      <p>In the meantime, if you wanna contact me about academic/research stuff email me at <a href="mailto:echacra@ucsd.edu">echacra@ucsd.edu</a></p>
+
+      <p>If you wanna contact me about music requests/collaborations/etc email me at <a href="mailto:music@emirchacra.com">music@emirchacra.com</a></p>
+
+      <p>You can also check my music at <a href="https://mncrmo.bandcamp.com">https://mncrmo.bandcamp.com</a></p>
+
+      <p>See you soon :)</p>
     </>
   )
 }
