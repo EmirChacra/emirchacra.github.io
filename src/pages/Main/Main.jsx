8d6a4a53c755f446f46c4e44ecb15a75f3047cad
CrisSaavedra <crisisaave@gmail.com>
2025-01-27 23:52:52 -0400

    feat(main): remove personal intro and contact information from Main component

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -4,27 +4,6 @@ function Main() {
     return (
         <>
             <Bio />
-            <p>
-                Hi,
-                <br />
-                I'm Emir Chacra, a Chilean music producer, bassist, DJ, and digital audio
-                researcher.
-            </p>
-            <p>
-                My webpage is under construction :) But soon it'll be online and beautiful
-                again.
-            </p>
-            <p>
-                In the meantime, if you wanna contact me about academic/research stuff email
-                me at <a href="mailto:echacra@ucsd.edu">echacra@ucsd.edu</a>
-            </p>
-            <p>
-                If you wanna contact me about music requests/collaborations/etc email me at <a href="mailto:music@emirchacra.com">music@emirchacra.com</a>
-            </p>
-            <p>
-                You can also check my music at <a href="https://mncrmo.bandcamp.com">https://mncrmo.bandcamp.com</a>
-            </p>
-            <p>See you soon :)</p>
 
         </>
     );
