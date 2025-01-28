6aaf12c7e40c01f8b29fe1b117ddd3fddacad65d
CrisSaavedra <crisisaave@gmail.com>
2025-01-28 12:22:35 -0400

    feat: restructure Main component, add StandardCard and clean up styles in App and index CSS files

---
+++ src/components/StandardCard/StandardCard.jsx
@@ -0,0 +1,28 @@
+import './StandardCard.css';
+
+
+function StandardCard() {
+    // const { img , title, links} = props;
+
+    return (
+        <section className='section-container'>
+            <img className='section-img' src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" alt="img" />
+            <div className='description-container'>
+                <h1 className='title'>Panoramas - mncrmo (LP, 2023)</h1>
+                <div className='text-container'>
+                    <p>Self-realesed</p>
+                    <p>Producer, bass player, and mixing engineer as mncrmo</p>
+                    <p>Master engineer - <a href="#">Gabo Araya</a></p>
+                </div>
+
+                <div className='links-container'>
+                    <a href="#">Listen on Bandcamp</a>
+                    {/* <a href="#">Link 2</a>
+                    <a href="#">Link 3</a> */}
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default StandardCard;
