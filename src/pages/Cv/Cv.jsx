0926375b9986a2042dbe8caeb1b7bcfba858e29e
EmirChacra <echacra@ucsd.edu>
2025-01-28 09:40:29 -0800

    Added CV section

---
+++ src/pages/Cv/Cv.jsx
@@ -0,0 +1,11 @@
+import cvpdf from "/src/assets/cv.pdf"
+
+function CV() {
+    return ( 
+        <div>
+        <a href = {cvpdf} target = "_blank">Download CV here.</a>
+        </div>
+     );
+}
+
+export default CV;
