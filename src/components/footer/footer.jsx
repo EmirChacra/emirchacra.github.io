8a6e9b725a966e4b3777decbcfc5f1fd841b9355
CrisSaavedra <crisisaave@gmail.com>
2025-01-31 12:01:47 -0300

    refactor(Footer): simplify structure by removing unnecessary div, update CSS for better styling and layout consistency

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -3,17 +3,14 @@ import './Footer.css';
 function Footer() {
     return (
         <footer className="footer">
-            <div>
-                <h3 className="footer-title">EMIR CHACRA</h3>
-                <section className="footer-content">
-                    <h4>Academic inquiries:</h4>
-                    <p>echacra@ucsd.edu</p>
+            <h3 className="footer-title">EMIR CHACRA</h3>
+            <section className="footer-content">
+                <h4>Academic inquiries:</h4>
+                <p>echacra@ucsd.edu</p>
 
-                    <h4>Music inquiries:</h4>
-                    <p>music@emirchacra.com</p>
-                </section>
-            </div>
-
+                <h4>Music inquiries:</h4>
+                <p>music@emirchacra.com</p>
+            </section>
         </footer>
     );
 }
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,3 +1,4 @@
+import Footer from "../../components/Footer/footer";
 import StandardCard from "../../components/StandardCard/StandardCard";
 // import Bio from "./components/Bio";
 import './Main.css';
@@ -7,6 +8,8 @@ function Main() {
         <div className="main-container">
             <StandardCard  title="Panoramas - mncrmo (LP, 2023)" img="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" texts={["Self-released", "Producer, bass player, and mixing engeneer as mncrmo", {text: "Master engineer - ", href: "www.google.com", textRef: "Gabo Araya."}]} links={{text: "Listen on bandcamp", href: "www.bandcamp.com"}} />
             {/* <Bio /> */}
+
+            <Footer />
         </div>
     );
 }
