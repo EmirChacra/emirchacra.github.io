4a7b202c83d0be337b68247095caeb2f38fe8ddb
CrisSaavedra <crisisaave@gmail.com>
2025-02-05 00:11:09 -0400

    Add Home component and styles for landing page layout

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,18 @@
+import './Home.css';
+
+function Home() {
+    return (
+        <main className="home-container">
+            <h1>emir chacra</h1>
+            <section className="links-container">
+                    <a href="https://github.com/emirchacra" target="_blank" rel="noopener noreferrer">about me</a>
+                    <a href="https://github.com/emirchacra" target="_blank" rel="noopener noreferrer">about me</a>
+                    <a href="https://github.com/emirchacra" target="_blank" rel="noopener noreferrer">about me</a>
+                    <a href="https://github.com/emirchacra" target="_blank" rel="noopener noreferrer">about me</a>
+                    <a href="https://github.com/emirchacra" target="_blank" rel="noopener noreferrer">about me</a>
+            </section>
+        </main>
+    );
+}
+
+export default Home;
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -4,10 +4,12 @@ import Footer from "../../components/Footer/Footer";
 import Bio from "./components/Bio";
 import ReleaseHighlight from "./components/ReleaseHighlight";
 import './Main.css';
+import Home from "../Home/Home";
 
 function Main() {
     return (
-        <section>
+        <section style={{paddingLeft: '5px', paddingRight: '5px'}}>
+            <Home/>
             <Bio/>
             <ReleaseHighlight/>
             <StandardCard></StandardCard>
