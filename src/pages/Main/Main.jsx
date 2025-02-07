2b02b59c34d46f259b159f3d29c0efcede354f2f
EmirChacra <echacra@ucsd.edu>
2025-02-06 18:33:01 -0800

    Leave only landing

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,7 +1,3 @@
-import Footer from '../../components/Footer/Footer';
-import AboutMe from '../AboutMe/AboutMe';
-import CV from '../Cv/Cv';
-import Music from '../Music/Music';
 import './Main.css';
 import Home from "../Home/Home";
 
@@ -9,12 +5,6 @@ function Main() {
     return (
         <>
             <Home />
-            <section style={{ paddingLeft: '5px', paddingRight: '5px' }}>
-                <Bio />
-                <ReleaseHighlight />
-                <StandardCard></StandardCard>
-                <Footer />
-            </section>
         </>
     );
 }
