7c7bf9d1faacac4774fb8d1f4a2a5a3d3316f4ff
CrisSaavedra <crisisaave@gmail.com>
2025-02-23 19:25:25 -0400

    Refactor MediaPlayer to use audio ref, handle play/pause logic, and include dynamic audio source with SVG fallback

--- src/components/MediaPlayer/MediaPlayer.jsx
+++ src/components/MediaPlayer/MediaPlayer.jsx
@@ -1,24 +1,39 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import "./MediaPlayer.css"
 import WaveSvg from "../WaveSvg/WaveSvg";
+import reverb from "../../assets/audio_files/ReverbGuitar.wav";
 
 
 
-export default function MediaPlayer() {
-    const [isPlaying, setIsPlaying] = useState(true);
+export default function MediaPlayer({ sound }) {
+    const audioRef = useRef(null);
+    const [isPlaying, setIsPlaying] = useState(false);
 
     function handlePlay() {
+        if (!audioRef) return;
+        if (isPlaying) {
+            audioRef.current.pause();
+            audioRef.current.currentTime = 0;
+        } else {
+            audioRef.current.play();
+        }
+
         setIsPlaying(!isPlaying);
     }
 
     return (
-        <button className="player-container" onClick={handlePlay}>
-            <source src="audio.mp3" type="audio/mpeg" />
-            {isPlaying ? <WaveSvg /> : <div/>}
-        </button>
+        <>
+            <button className="player-container" onClick={handlePlay}>
+                {isPlaying ? <WaveSvg /> : <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" ><polygon points="6 3 20 12 6 21 6 3" /></svg>
+                }
+            </button>
+            <audio ref={audioRef} src={sound ? sound : reverb} onEnded={handlePlay}></audio>
+        </>
     )
 
 }
+
+
 
 
 
