f3e86faaec6a7a3a650556e57187b9bbda8b2b1c
CrisSaavedra <crisisaave@gmail.com>
2025-03-04 23:40:38 -0300

    Refactor Music styles for a more responsive layout and enhance image hover effects in Music component

--- src/pages/Music/Music.jsx
+++ src/pages/Music/Music.jsx
@@ -1,14 +1,5 @@
 import "./Music.css";
 
-import { useEffect, useState } from 'react';
-
-import "../../styles/Background.css";
-import background1 from "../../assets/backgrounds/homeBackground1.jpg";
-import background2 from "../../assets/backgrounds/homeBackground2.jpg";
-import background3 from "../../assets/backgrounds/homeBackground3.jpg";
-import background4 from "../../assets/backgrounds/homeBackground4.jpg";
-import background5 from "../../assets/backgrounds/homeBackground5.jpg";
-
 import earlyRiser from "../../assets/earlyRiser_cover.jpg";
 import neonatologia from "../../assets/neonatologia_cover.jpg";
 import panoramas from "../../assets/panoramas_cover.jpg";
@@ -19,7 +10,7 @@ const backgroundImages = [earlyRiser, neonatologia, panoramas, ambientes];
 
 function Music() {
     
-    const images = backgroundImages.map( (image, index) => <img className='musicImages' key={index} src={image} ></img> )
+    const images = backgroundImages.map( (image, index) =>  <img className='musicImages' key={index} src={image} ></img> )
 
     return ( 
         <section className='musicSection'>
