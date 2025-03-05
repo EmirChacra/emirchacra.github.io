089dfefa983c3fce040bc12b12f58e8f34c71896
CrisSaavedra <crisisaave@gmail.com>
2025-03-05 00:26:56 -0300

    Add ModalBase component and integrate it into Music page for improved image interaction with modal display functionality

---
+++ src/components/ModalBase/ModalBase.jsx
@@ -0,0 +1,34 @@
+import { useEffect } from "react";
+import ReactDOM from "react-dom";
+import "./ModalBase.css";
+
+function ModalBase({ children, onClose }) {
+    const modalRoot = document.getElementById("modal-root");
+    const el = document.createElement("div");
+
+    useEffect(() => {
+        // Agregar el elemento al modal-root
+        modalRoot.appendChild(el);
+
+        // Limpiar el elemento cuando el componente se desmonte
+        return () => {
+            modalRoot.removeChild(el);
+        };
+    }, [el, modalRoot]);
+
+    return ReactDOM.createPortal(
+        <div className="modalDisplay">
+            <div className="modalContainer">
+                <header className="modalHeader">
+                    <button onClick={onClose}>X</button>
+                </header>
+                <div className="modalContent" >
+                    {children}
+                </div>
+            </div>
+        </div>,
+        el
+    );
+}
+
+export default ModalBase;
--- src/pages/Music/Music.jsx
+++ src/pages/Music/Music.jsx
@@ -4,18 +4,34 @@ import earlyRiser from "../../assets/earlyRiser_cover.jpg";
 import neonatologia from "../../assets/neonatologia_cover.jpg";
 import panoramas from "../../assets/panoramas_cover.jpg";
 import ambientes from "../../assets/ambientes_cover.jpg";
+import { useState } from "react";
+import ModalBase from "../../components/ModalBase/ModalBase.JSX";
+
 
 const backgroundImages = [earlyRiser, neonatologia, panoramas, ambientes];
 
 
 function Music() {
-    
-    const images = backgroundImages.map( (image, index) =>  <img className='musicImages' key={index} src={image} ></img> )
 
-    return ( 
-        <section className='musicSection'>
-           {images}
-        </section>
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const openModal = () => setIsModalOpen(true);
+    const closeModal = () => setIsModalOpen(false);
+
+    const images = backgroundImages.map((image, index) => <img className='musicImages' key={index} src={image} onClick={openModal}></img>)
+
+    return (
+        <>
+            <section className='musicSection'>
+                {images}
+            </section>
+            {
+                isModalOpen &&
+                <ModalBase onClose={closeModal}>
+
+                </ModalBase>
+            }
+        </>
     );
 }
 
