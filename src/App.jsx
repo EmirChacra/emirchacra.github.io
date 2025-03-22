a17b628265e1895b9320c49aa51787684ed9e37a
CrisSaavedra <crisisaave@gmail.com>
2025-03-21 22:11:12 -0400

    refactor: move background logic to BackgroundView component
    Extracted background image handling and animation logic from Homev2, Cv, and App components into a centralized BackgroundView component. This improves code maintainability and reduces redundancy by consolidating background-related functionality in one place.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import { Outlet } from 'react-router'
 import './App.css'
+import BackgroundView from './components/BackgroundView/BackgroundView'
+import { motion } from "motion/react";
 
 
 function App() {
 
   return (
-    <Outlet/>
+    <motion.div initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 1.5 }}>
+      <BackgroundView />
+      <Outlet />
+    </motion.div>
   )
 }
 
--- src/components/BackgroundView/BackgroundView.jsx
+++ src/components/BackgroundView/BackgroundView.jsx
@@ -1,7 +1,51 @@
+import { useEffect, useState } from "react";
+import hbg1 from "../../assets/backgrounds/h1.jpg";
+import hbg2 from "../../assets/backgrounds/h2.jpg";
+import hbg3 from "../../assets/backgrounds/h3.jpg";
+import wbg1 from "../../assets/backgrounds/w1.jpg";
+import wbg2 from "../../assets/backgrounds/w2.jpg";
+import wbg3 from "../../assets/backgrounds/w3.jpg";
+
 function BackgroundView() {
+    let backgroundImages;
+
+    const [containerWidth, setContainerWidth] = useState(window.innerWidth);
+    const [containerHeight, setContainerHeight] = useState(window.innerHeight);
+
+    // Array of background images
+    if (containerHeight >= containerWidth) {
+        backgroundImages = [hbg1, hbg2, hbg3];
+    }
+    else {
+        backgroundImages = [wbg1, wbg2, wbg3];
+    }
+
+    const bgImg = backgroundImages[Math.floor(Math.random() * backgroundImages.length)]
+
+
+
+    useEffect(() => {
+        // Resize listener to update container width and height
+        const handleResize = () => {
+            setContainerWidth(window.innerWidth);
+            setContainerHeight(window.innerHeight);
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
     return (
-        <>
-        </>
+        <div className="background-container"
+            style={{
+                backgroundImage: `url(${bgImg})`,
+                opacity: 0.2
+            }}
+        >
+        </div>
     );
 }
 
