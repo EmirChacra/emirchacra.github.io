0bff582add1617d6b37fcfa893ce3f446b05038b
CrisSaavedra <crisisaave@gmail.com>
2025-03-28 22:35:45 -0300

    feat(SlideImages): integrate Swiper for image slideshow
    Add Swiper library to enable a responsive and interactive image slideshow in the SlideImages component. This includes updating CSS and JSX to support Swiper's features like coverflow effect and pagination. The package.json and package-lock.json files are also updated to include the Swiper dependency.

--- src/pages/Music/components/SlideImages/SlideImages.jsx
+++ src/pages/Music/components/SlideImages/SlideImages.jsx
@@ -1,10 +1,39 @@
 import './SlideImages.css';
-function SlideImages() {
-    return ( 
+import { EffectCoverflow, Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+import earlyRiser from "../../../../assets/album_covers/earlyRiser_cover.jpg";
+
+function SlideImages(data) {
+    return (
         <div className="slide-images">
-            
-        </div>
-     );
+
+            <Swiper
+                modules={[EffectCoverflow, Pagination]}
+                spaceBetween={0}
+                slidesPerView={1}
+                effect={'coverflow'}
+                coverflowEffect={{
+                    rotate: 0,
+                    stretch: 0,
+                    depth: 100,
+                    modifier: 1,
+                    slideShadows: false,
+                }}
+                centeredSlides={'auto'}
+                onSlideChange={() => console.log('slide change')}
+                onSwiper={(swiper) => console.log(swiper)}
+                pagination={{ clickable: true }}
+            >
+                <SwiperSlide><img src={earlyRiser} alt="cover" /></SwiperSlide>
+                <SwiperSlide>Slide 2</SwiperSlide>
+                <SwiperSlide>Slide 3</SwiperSlide>
+                <SwiperSlide>Slide 4</SwiperSlide>
+            </Swiper>
+        </div >
+    );
 }
 
 export default SlideImages;
