3bacfb6d00797e2bc681249e9717448fc0ab5d42
EmirChacra <echacra@ucsd.edu>
2025-04-13 20:30:54 -0700

    Music section styling

--- src/pages/Music/components/InfoDisplay/InfoDisplay.jsx
+++ src/pages/Music/components/InfoDisplay/InfoDisplay.jsx
@@ -6,39 +6,20 @@ function InfoDisplay(item) {
 
     const showElement = ({ item }) => {
         return (
-            <>
+            <div className="info-display">
                 <div className="content-info-display">
-                    <h3>{item.title}</h3>
-                    {
-                        item.text.map((text, index) => {
-                            if (text.text_linked === '') {
-                                return (
-                                    <p key={'text' + index}>{text.text}</p>
-                                )
-                            } else {
-                                return (
-                                    <div key={index}>
-                                        <p>{text.text} <Link to={text.link}>{text.text_linked}</Link></p>
-                                    </div>
-                                )
-                            }
-
-                        })
-                    }
+                    <h2 className="title">{item.title}</h2>
+                    <>{item.description}</>
                 </div>
-                <div>
-                    <img src={item.img} alt="cover" />
-                </div>
-            </>
+                <img className='img' src={item.img} alt="Cover" />
+            </div>
         )
     }
 
 
 
     return (
-        <div className="info-display">
-            {showElement(item)}
-        </div>
+        <> {showElement(item)} </>
     );
 }
 
