a41e0fdc5ab659b2f38f30ddc1d5076c913574df
CrisSaavedra <crisisaave@gmail.com>
2025-06-11 16:44:04 -0400

    style: update cv font size

--- src/pages/Cv/components/Publications.jsx
+++ src/pages/Cv/components/Publications.jsx
@@ -1,23 +1,26 @@
-
 function Publications() {
-    return (
+  return (
+    <div className="text-container">
+      <h4>Peer-reviewed journal articles</h4>
+      <ul>
+        <li>
+          S. Gonzalez, <b>E. Chacra</b>, C. Carreño, C. Espinoza, “Wooden
+          Mechanical Metamaterials: towards tunable wood plates”, Materials and
+          Design, 221, 110952, 2022.
+        </li>
+      </ul>
 
-        <div className="text-container">
-            <h4>Peer-reviewed journal articles</h4>
-            <ol>
-                <li>S. Gonzalez, <b>E. Chacra</b>, C. Carreño, C. Espinoza, “Wooden Mechanical Metamaterials:
-                    towards tunable wood plates”, Materials and Design, 221, 110952, 2022.</li>
-            </ol>
-
-            <h4>Conference proceedings</h4>
-            <ol>
-                <li>C. Espinoza, C. Carreño, <b>E. Chacra</b>, S. Gonzalez, “MetaWood: manipulation of the
-                    elastic properties of wood plates by periodic hole patterns”, 2022 Sixteenth International
-                    Congress on Artificial Materials for Novel Wave Phenomena (Metamaterials), 2022, pp.
-                    139-141.</li>
-            </ol>
-        </div>
-    );
+      <h4>Conference proceedings</h4>
+      <ul>
+        <li>
+          C. Espinoza, C. Carreño, <b>E. Chacra</b>, S. Gonzalez, “MetaWood:
+          manipulation of the elastic properties of wood plates by periodic hole
+          patterns”, 2022 Sixteenth International Congress on Artificial
+          Materials for Novel Wave Phenomena (Metamaterials), 2022, pp. 139-141.
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default Publications;
